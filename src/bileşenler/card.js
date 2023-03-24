dd8fe208d241c48d670a44875e2867150f2dfe62
cosmopoliitans <elfkyn52@gmail.com>
2023-03-25 01:11:43 +0300

    Cards bitti

--- src/bileşenler/card.js
+++ src/bileşenler/card.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const Card = (makale) => {
   // GÖREV 5
   // ---------------------
@@ -17,7 +19,38 @@ const Card = (makale) => {
   //   </div>
   // </div>
   //
-}
+  const cardBox = document.createElement("div");
+  cardBox.classList.add("card");
+
+  const anabaslikBox = document.createElement("div");
+  anabaslikBox.classList.add("headline");
+  anabaslikBox.textContent = makale.anabaslik;
+  cardBox.appendChild(anabaslikBox);
+
+  const authorBox = document.createElement("div");
+  authorBox.classList.add("author");
+  cardBox.appendChild(authorBox);
+
+  const imgContainer = document.createElement("div");
+  imgContainer.classList.add("img-container");
+  authorBox.appendChild(imgContainer);
+
+  const yazarFotoImg = document.createElement("img");
+  yazarFotoImg.src = makale.yazarFoto;
+  imgContainer.appendChild(yazarFotoImg);
+
+  const yazarAdi = document.createElement("span");
+  yazarAdi.textContent = makale.yazarAdi + " tarafından";
+  authorBox.appendChild(yazarAdi);
+
+  authorBox.appendChild(yazarAdi);
+  cardBox.appendChild(authorBox);
+
+  cardBox.addEventListener("click", (event) => {
+    console.log(makale.anabaslik);
+  });
+  return cardBox;
+};
 
 const cardEkleyici = (secici) => {
   // GÖREV 6
@@ -28,6 +61,22 @@ const cardEkleyici = (secici) => {
   // Card bileşenini kullanarak yanıttaki her makale nesnesinden bir kart oluşturun.
   // Her cardı, fonksiyona iletilen seçiciyle eşleşen DOM'daki öğeye ekleyin.
   //
-}
+  const newSelector3 = document.querySelector(secici);
 
-export { Card, cardEkleyici }
+  axios
+    .get("http://localhost:5001/api/makaleler")
+
+    .then((response) => {
+      console.log(response.data.makaleler);
+      for (let key in response.data.makaleler) {
+        for (let i = 0; i < response.data.makaleler[key].length; i++) {
+          newSelector3.appendChild(Card(response.data.makaleler[key][i]));
+        }
+      }
+    })
+    .catch((error) => {
+      console.log("Error: " + err);
+    });
+};
+
+export { Card, cardEkleyici };
--- src/bileşenler/tablar.js
+++ src/bileşenler/tablar.js
@@ -1,4 +1,4 @@
-import { axios } from "axios";
+import axios from "axios";
 
 const Tablar = (konu) => {
   // GÖREV 3
